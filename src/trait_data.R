2aa35f8b5390dbfd16ff8e7fd868546112ff49f5
Brannon Basilio <bbasilio808@gmail.com>
2018-05-22 21:52:54 +0000

    This code is for the data for canopy cover.

---
+++ src/trait_data.R
@@ -0,0 +1,21 @@
+library(traits)
+
+variables <- betydb_query(table = 'search', key = readLines('~/.betykey'), betyurl = 'https://terraref.ncsa.illinois.edu/bety')
+variables <- betydb_query(table = 'variables',
+                          key = readLines('~/.betykey'),
+                          betyurl = 'https://terraref.ncsa.illinois.edu/bety')
+
+options(betydb_key = readLines('~/.betykey', warn = FALSE),
+        betydb_url = "https://terraref.ncsa.illinois.edu/bety/",
+        betydb_api_version = 'beta')
+
+canopy_cover <- betydb_query(table = 'search',
+                             trait = "canopy_cover",
+                             site  = "~Season 6",
+                             limit = 'none')
+
+write.csv(canopy_cover, file = 'data/canopy_cover.csv')
+dir.create('data')
+dir.create('doc')
+dir.create('bin')
+dir.create('results')
