f08f862cf22ca859d403a7fdd61fa446d00c1d89
David LeBauer <dlebauer@illinois.edu>
2018-05-26 11:56:02 +0000

    visualizing maps after joining

--- src/Joining_table_polys.R
+++ src/Joining_table_polys.R
@@ -34,19 +34,21 @@ sites_poly <- do.call("rbind", filter(site_geom, geom_type == "SpatialPolygons")
 sites_point <- do.call("rbind", filter(site_geom, geom_type == "SpatialPoints")$parsed_geometry)
 
 
-readr::write_csv(CC_date_sites, 'data/canopy_cover_geoms.csv')
 leaflet() %>% addTiles() %>% addPolygons(data = sites_poly, color = "red")
 
 
 #downloading canopy cover data and saving them
 
+options(betydb_key = readLines('~/.betykey', warn = FALSE),
+        betydb_url = "https://terraref.ncsa.illinois.edu/bety/",
+        betydb_api_version = 'beta')
 
 canopy_cover <- betydb_query(table = 'search',
                              trait = "canopy_cover",
                              site  = "~Season 6",
                              limit = "none")
 
-write.csv(canopy_cover, file = 'data/canopy_cover.csv')
+#write.csv(canopy_cover, file = 'data/canopy_cover.csv')
 
 
 # joing canopy cover and poly data
@@ -55,6 +57,14 @@ CC_date <- canopy_cover %>% group_by(date)
 
 CC_date_sites <- CC_date %>% left_join(site_geom, by = 'site_id') %>% 
   dplyr::select(date, raw_date, cultivar, method_name, canopy_cover = mean, sitename, parsed_geometry)
+
+#readr::write_csv(CC_date_sites, 'data/canopy_cover_geoms.csv')
+
+#visualizing 
+
+leaflet() %>% addTiles() %>% 
+  addPolygons(data = CC_date_sites$parsed_geometry, 
+              color = "red")
 
 
 
